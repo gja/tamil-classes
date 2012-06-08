c72f587973dd1b834cb51e5c0c49408d72207a31
Tejas Dinkar <tejas@gja.in>
2012-06-08 20:46:04 +0530

    Initial commit

---
+++ app/admin/chapters.rb
@@ -0,0 +1,2 @@
+ActiveAdmin.register Chapter do  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TamilClasses::Application.config.secret_token = '72f6e2417354332ed806b4b0c195e511071cf5313d63ea38380f94bc25ef848f704493b943966223d7dea11971d4804cb1f8d9e8750a38c994b1562ec7d0554a'
